73d114f20a6f791ea4036d7a5411474c9e39141e
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2022-09-03 17:22:10 +0530

    foodcard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import FoodCard from './components/FoodCard';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar />
     <FoodCard />
     </>
   );
--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -3,45 +3,32 @@ import React from "react";
 const FoodCard = () => {
   return (
     <div>
-      
-<nav className="bg-lime-500 border-gray-200 sm:px-4  dark:bg-gray-900">
-  <div className="container flex flex-wrap justify-between items-center mx-auto">
-    <div className="navbar flex justify-between items-center md:hidden">
-        <div className="flex justify-center items-center">
-            <div className="sm:hamburger inline-block p-4 cursor-pointer">
-                <div className="line h-0.5 w-6 my-1 bg-black"></div>
-                <div className="line h-0.5 w-6 my-1 bg-black"></div>
-                <div className="line h-0.5 w-6 my-1 bg-black"></div>
-            </div>
-        </div>
-    </div>
-    <div className="w-full md:block md:flex-row -translate-y-6 md:translate-y-0">   
-    <ul className="flex flex-col md:p-2 mt-4 sm:space-x-18 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="#" className="block md:py-2 pr-4 pt--10  pl-3 text-white rounded md:bg-transparent md:text-gray-300-700 md:p-0 dark:text-white">Home</a>
-        </li>
-        <li>
-          <a href="#" className="block md:py-2 pr-4 pl-3 text-black-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-        </li>
-        <li>
-          <a href="#" className="block md:py-2 pr-4 pl-3 text-black-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" className="block md:py-2 pr-4 pl-3 text-black-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" className="block md:py-2 pr-4 pl-3 text-black-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
       <h1 className="mt-8 text-center text-lime-300 text-3xl font-bold">
         Food Ordering Page
       </h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/* Card starts */}
+    
+      <a href="#" className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row w-26 h-40 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <img className="object-cover w-40 h-26 rounded-t-lg md:h-40 md:w-26 md:rounded-none md:rounded-l-lg" src="https://www.eatthis.com/wp-content/uploads/sites/4/2019/09/plate-penne-pasta-tomato-sauce.jpg?quality=82&strip=1" alt=""/>
+        <div className="flex flex-col justify-between p-4 leading-normal">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Mexican Pizza       </h5>
+          <tr>
+            <td className="mr-8">sd</td>
+            <td>Price</td>
+          </tr>
+          <tr>
+            <td>Quantity</td>
+          </tr>
+
+          <div class="flex justify-between items-center">
+            <span class="text-xl space-x-18 font-bold text-gray-900 dark:text-white">$599</span>
+            <a href="#" class="text-white space-x-18 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+        </div>
+        </div>
+      </a>
+
+      {/* Card ends */}
     </div>
   );
 };
